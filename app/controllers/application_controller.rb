b0953ef1eeb171fe81b4f74eae1ce6a1cb02e12e
Dmitry Syvoglaz <grayeyed16@gmail.com>
2019-03-21 14:59:52 +0200

    add :name to sign up strong params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@
 
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  respond_to :json
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
