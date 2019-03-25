e5b65b8746b02518817be3ffda80f8791963244b
Dmitry Syvoglaz <grayeyed16@gmail.com>
2019-03-25 13:02:05 +0200

    Add index to project_users table's columns

--- db/migrate/20190325104826_create_project_users.rb
+++ db/migrate/20190325104826_create_project_users.rb
@@ -1,8 +1,8 @@
 class CreateProjectUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :project_users do |t|
-      t.references :user, foreign_key: true
-      t.references :project, foreign_key: true
+      t.references :user, foreign_key: true, index: true
+      t.references :project, foreign_key: true, index: true
 
       t.timestamps
     end
