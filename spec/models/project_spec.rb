af8d32b533a7d736e25fbd3fcda7f9910848b058
Dmitry Syvoglaz <grayeyed16@gmail.com>
2019-03-26 11:42:53 +0200

    Add project spec for dependent destroy

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -5,5 +5,11 @@ require 'rails_helper'
 RSpec.describe Project, type: :model do
   let(:project) { create(:project) }
 
-  it { should validate_presence_of(:name) }
+  context 'should have unique name' do
+    it { should validate_presence_of(:name) }
+  end
+
+  context 'should destroy entries in project_users table with foreign key' do
+    it { expect(project).to have_many(:project_users).dependent(:destroy) }
+  end
 end
