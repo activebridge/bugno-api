bb4fceeeac77ec75a7e903b785e25b25c8597c72
Dmitry Syvoglaz <grayeyed16@gmail.com>
2019-04-10 13:20:06 +0300

    Add new attributes to event serializer

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class EventSerializer < ApplicationSerializer
-  attributes :id, :title, :environment, :status, :user_id, :project_id
+  attributes :id, :title, :environment, :status, :user_id, :project_id,
+             :message, :backtrace, :framework, :url, :user_ip, :headers,
+             :method, :params
 end
