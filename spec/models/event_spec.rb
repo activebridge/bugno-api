aaf7d178e863c52e1817d68cad919f85916907c5
Dmitry Syvoglaz <grayeyed16@gmail.com>
2019-04-26 17:52:05 +0300

    Change backtrace type to array of jsonb, add server data to events

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -3,8 +3,6 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  let(:event) { create(:event) }
-
   context 'should has title and status' do
     it { should validate_presence_of(:title) }
     it { should validate_presence_of(:status) }
