06cc8f07d3dc3bd44e874096eefb3c9908571f1b
Dmitry Syvoglaz <grayeyed16@gmail.com>
2019-05-07 14:25:21 +0300

    Fix mailer configuration in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,8 +63,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
-    user_name: ENV = ['MAILER_DELIVERY_SERVICE_USERNAME'],
-    password: ENV = ['MAILER_DELIVERY_SERVICE_PASSWORD'],
+    user_name: ENV['MAILER_DELIVERY_SERVICE_USERNAME'],
+    password: ENV['MAILER_DELIVERY_SERVICE_PASSWORD'],
     authentication: 'plain',
     enable_starttls_auto: true
   }
