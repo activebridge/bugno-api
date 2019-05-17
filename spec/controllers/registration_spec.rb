0a6293f5dbf076767f6801702ba971bd704254db
Dmitry Syvoglaz <grayeyed16@gmail.com>
2019-05-17 16:25:32 +0300

    Add update user name

--- spec/controllers/registration_spec.rb
+++ spec/controllers/registration_spec.rb
@@ -4,14 +4,28 @@ require 'rails_helper'
 
 describe 'registration', type: :request do
   let(:user_attributes) { attributes_for(:user) }
+  let(:base_url) { user_registration_path }
+  let(:url) { base_url }
+  let(:params) { {} }
+  let(:request_params) { [url, { params: params, headers: headers }] }
 
   context '#create' do
+    let(:params) { user_attributes }
+    subject { -> { post(*request_params) } }
+
+    it { is_expected.to change(User, :count).by(1) }
+  end
+
+  context '#update' do
+    let!(:user) { create(:user) }
+    let(:headers) { user.create_new_auth_token }
+    let(:params) { user_attributes }
+
     subject do
-      post user_registration_path, params: user_attributes
-      response
+      login(user)
+      put(*request_params)
     end
 
-    it { is_expected.to have_http_status(200) }
-    it { expect { subject }.to change(User, :count).by(1) }
+    it { expect { subject }.to change { user.reload.attributes } }
   end
 end
