951b3e22c9b1ca3d78f881f54cf1a4c625691cf0
Dmitry Syvoglaz <grayeyed16@gmail.com>
2019-07-02 11:27:33 +0300

    Exclude third party software pathes from project error trace check

--- app/services/events/parent_create_service.rb
+++ app/services/events/parent_create_service.rb
@@ -1,10 +1,13 @@
 # frozen_string_literal: true
 
 class Events::ParentCreateService < ApplicationService
+  EXLCUDED_DIRECTORIES = ['vendor/', 'node_modules/']
+
   def call
     if event['framework'] == 'browser-js'
       browser_js_parent&.id
     else
+      assign_project_error_trace
       project_error_trace && project_error_trace['code'] ? parent_by_backtrace&.id : parent_by_title&.id
     end
   end
@@ -22,13 +25,26 @@ class Events::ParentCreateService < ApplicationService
     project.events.find_by(title: event['title'], message: event['message'], parent_id: nil)
   end
 
-  def project_error_trace
-    event['backtrace'].find do |trace|
-      trace['filename'].include?(event['server_data']['root'])
+  def browser_js_parent
+    project.events.find_by(message: event['message'], backtrace: event['backtrace'], parent_id: nil)
+  end
+
+  def assign_project_error_trace
+    event['backtrace'].map do |trace|
+      trace['project_error'] = true if trace['filename'].include?(event['server_data']['root']) && \
+                                       !excluded_directory?(trace['filename'])
     end
   end
 
-  def browser_js_parent
-    project.events.find_by(message: event['message'], backtrace: event['backtrace'], parent_id: nil)
+  def project_error_trace
+    event['backtrace'].find do |trace|
+      trace['project_error']
+    end
+  end
+
+  def excluded_directory?(filename)
+    EXLCUDED_DIRECTORIES.any? do |directory|
+      filename.include?(directory)
+    end
   end
 end
