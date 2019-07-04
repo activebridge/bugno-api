fa8701b0fa13acabc303722b5b4e2dff8e126f0b
Dmitry Syvoglaz <grayeyed16@gmail.com>
2019-07-04 20:40:19 +0300

    Add subscription to project serializer, fix events create service

--- app/serializers/project_serializer.rb
+++ app/serializers/project_serializer.rb
@@ -3,6 +3,7 @@
 class ProjectSerializer < ApplicationSerializer
   attributes :id, :name, :description, :api_key, :slug
   attribute :stripe_public_key, if: proc { instance_options[:include_stripe_api_key] }
+  has_one :subscription
 
   def stripe_public_key
     ENV['STRIPE_DEVELOPMENT_PUBLIC_KEY']
--- app/services/events/create_service.rb
+++ app/services/events/create_service.rb
@@ -13,11 +13,12 @@ class Events::CreateService < ApplicationService
   def handle_event_create
     EventMailer.create(event).deliver_later if project && event.persisted?
     Subscription.decrement_counter(:events, subscription.id)
+    subscription.expired! if subscription.events <= 1
     [{ message: I18n.t('api.event_captured') }, 201]
   end
 
   def expired_subscription
-    subscription.expired? || subscription.events.negative?
+    subscription.expired? || !subscription.events.positive?
   end
 
   def project
