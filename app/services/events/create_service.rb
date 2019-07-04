3d68676de78b80f59d78c5eb2ff7ead9371e3632
Dmitry Syvoglaz <grayeyed16@gmail.com>
2019-07-04 13:55:38 +0300

    Add verbose response to events create end point

--- app/services/events/create_service.rb
+++ app/services/events/create_service.rb
@@ -2,7 +2,8 @@
 
 class Events::CreateService < ApplicationService
   def call
-    return if subscription.nil? || expired_subscription
+    return [{ message: I18n.t('api.errors.invalid_api_key') }, 401] unless project
+    return [{ message: I18n.t('api.errors.subscription_absent') }, 422] if subscription.nil? || expired_subscription
 
     handle_event_create
   end
@@ -12,6 +13,7 @@ class Events::CreateService < ApplicationService
   def handle_event_create
     EventMailer.create(event).deliver_later if project && event.persisted?
     Subscription.decrement_counter(:events, subscription.id)
+    [{ message: I18n.t('api.event_captured') }, 201]
   end
 
   def expired_subscription
@@ -19,7 +21,7 @@ class Events::CreateService < ApplicationService
   end
 
   def project
-    @project ||= Project.find_by!(api_key: declared_params[:project_id])
+    @project ||= Project.find_by(api_key: declared_params[:project_id])
   end
 
   def event
