fa297c2f07f34eef719b0a8fceb407e2a06a51dd
Dmitry Syvoglaz <grayeyed16@gmail.com>
2019-07-23 14:26:05 +0300

    Create and update events with websockets

--- app/models/event.rb
+++ app/models/event.rb
@@ -15,10 +15,19 @@ class Event < ApplicationRecord
   scope :by_parent, ->(parent_id) { where(parent_id: parent_id) if parent_id.present? }
 
   before_create :assign_parent
+  after_save :brodcast
 
   private
 
   def assign_parent
     self.parent_id = ::Events::ParentCreateService.call(event: self, project: project)
+  end
+
+  def brodcast
+    action = saved_change_to_id? ? UserChannel::ACTIONS::CREATE_EVENT : UserChannel::ACTIONS::UPDATE_EVENT
+    project.project_users.each do |project_user|
+      ActionCable.server.broadcast("user_#{project_user.user_id}",
+                                   EventSerializer.new(self, action: action).as_json)
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,12 @@ Rails.application.routes.draw do
   rescue StandardError
     ActiveAdmin::DatabaseHitDuringLoad
 end
+
+  mount ActionCable.server, at: '/cable'
   mount API::Base, at: '/api'
   mount GrapeSwaggerRails::Engine, at: '/documentation'
   mount_devise_token_auth_for 'User', at: '/auth', controllers: {
-    omniauth_callbacks: 'omniauth_callbacks'
+    omniauth_callbacks: 'omniauth_callbacks',
+    token_validations: 'token_validations'
   }
 end
