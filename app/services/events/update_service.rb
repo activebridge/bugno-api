0e8d6296a4812e33fc029dfcdad11b88d0f984d5
Dmitry Sivoglaz <grayeyed16@gmail.com>
2019-08-05 14:20:35 +0300

    Refactor create_activity for events update_service

--- app/services/events/update_service.rb
+++ app/services/events/update_service.rb
@@ -4,18 +4,18 @@ class Events::UpdateService < ApplicationService
   def call
     return unless event.update(declared_params[:event])
 
-    event.create_activity(:update, owner: user,
-                                   recipient: project,
-                                   params: { status: { previous: changes['status'].first,
-                                                       new: changes['status'].last } })
-    occurrences.update_all(status: declared_params[:event][:status]) if declared_params[:event][:status].present?
+    create_activity
+    occurrences.update_all(status: declared_params[:event][:status]) if declared_params[:event][:status]
     event
   end
 
   private
 
-  def changes
-    @changes ||= event.saved_changes
+  def create_activity
+    event.create_activity(:update, owner: user,
+                                   recipient: project,
+                                   params: { status: { previous: event.saved_changes['status'].first,
+                                                       new: event.saved_changes['status'].last } })
   end
 
   def event
@@ -23,7 +23,7 @@ class Events::UpdateService < ApplicationService
   end
 
   def occurrences
-    @occurrences ||= project.events.where(parent_id: declared_params[:id])
+    @occurrences ||= event.occurrences
   end
 
   def project
