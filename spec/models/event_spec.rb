d1983c427ba29c76233507c5977814e6517cb78a
Dmitry Sivoglaz <grayeyed16@gmail.com>
2019-08-11 16:53:53 +0300

    Add association examples to event model spec

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -3,12 +3,11 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  context 'should has title and status' do
-    it { should validate_presence_of(:title) }
-    it { should validate_presence_of(:status) }
-  end
-
-  context 'belongs to project' do
-    it { should belong_to(:project) }
-  end
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:status) }
+  it { should validate_presence_of(:framework) }
+  it { should belong_to(:project) }
+  it { should belong_to(:user).optional }
+  it { should belong_to(:parent).class_name('Event').counter_cache(:occurrence_count).optional }
+  it { should have_many(:occurrences).class_name('Event').with_foreign_key('parent_id') }
 end
