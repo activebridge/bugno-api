b4c693651d4222a54969045863b87f5a512edf53
Dmitry Sivoglaz <grayeyed16@gmail.com>
2019-08-12 10:37:40 +0300

    Add activities assocition to project and user

--- app/models/project.rb
+++ app/models/project.rb
@@ -8,6 +8,7 @@ class Project < ApplicationRecord
   has_many :users, through: :project_users
   has_many :events, dependent: :delete_all
   has_many :active_events, -> { where(status: :active, parent_id: nil) }, class_name: 'Event'
+  has_many :activities, class_name: 'PublicActivity::Activity', as: :recipient
   has_one :subscription, dependent: :destroy
 
   friendly_id :name, use: %i[slugged finders]
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,5 @@ class User < ActiveRecord::Base
 
   has_many :project_users, dependent: :destroy
   has_many :projects, through: :project_users
+  has_many :project_activities, through: :projects, source: :activities
 end
