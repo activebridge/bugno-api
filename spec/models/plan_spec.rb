5667d810ebbfb276067cc774bb752d1753687985
Dmitry <grayeyed16@gmail.com>
2019-08-12 10:03:54 +0300

    Refactor model specs (#66)
    * Add validation of framework presence to event model
    
    * Add association examples to event model spec
    
    * Add association examples to project model spec
    
    * Refactor subscription model spec
    
    * Refactor plan model spec

--- spec/models/plan_spec.rb
+++ spec/models/plan_spec.rb
@@ -3,12 +3,8 @@
 require 'rails_helper'
 
 RSpec.describe Plan, type: :model do
-  context 'validates presence' do
-    it { should validate_presence_of(:name) }
-    it { should validate_presence_of(:price) }
-  end
-
-  context 'validates uniqueness' do
-    it { should validate_uniqueness_of(:name) }
-  end
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:price) }
+  it { should validate_uniqueness_of(:name) }
+  it { should have_many(:subscriptions).dependent(:destroy) }
 end
