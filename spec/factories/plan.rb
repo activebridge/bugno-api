50973cb2889ab99d346e6405b09e5248ce0f7af9
Dmitry Sivoglaz <grayeyed16@gmail.com>
2019-08-24 12:52:54 +0300

    Update faker invocations in factories

--- spec/factories/plan.rb
+++ spec/factories/plan.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
   factory :plan do
     name { Faker::App.unique.name }
     description { Faker::Books::Lovecraft.sentence }
-    price { Faker::Number.decimal(2) }
-    event_limit { Faker::Number.number(5) }
+    price { Faker::Number.decimal(l_digits: 2) }
+    event_limit { Faker::Number.number(digits: 5) }
   end
 end
