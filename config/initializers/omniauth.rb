158392f368ab51d15c5e1495577f111c83ce495e
Dmitry Sivoglaz <grayeyed16@gmail.com>
2019-08-28 10:52:48 +0300

    Fix routes mapping, increase batch_request_buffer_throttle to 10 sec

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,5 +3,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], scope: 'user:email'
   provider :slack, ENV['SLACK_KEY'], ENV['SLACK_SECRET'], scope: 'incoming-webhook,team:read'
-  on_failure { |env| DeviseTokenAuth::OmniauthCallbacksController.action(:omniauth_failure).call(env) }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ end
   mount ActionCable.server, at: '/cable'
   mount API::Base, at: '/api'
   mount GrapeSwaggerRails::Engine, at: '/documentation'
-  mount_devise_token_auth_for 'User', at: '/auth', controllers: {
+  mount_devise_token_auth_for 'User', at: 'auth', controllers: {
     omniauth_callbacks: 'omniauth_callbacks',
     token_validations: 'token_validations'
   }
