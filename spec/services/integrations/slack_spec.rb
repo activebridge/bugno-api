2b39a65bc1adc0caf856d10fc18423ea7e7fd90a
Dmitry Sivoglaz <grayeyed16@gmail.com>
2019-08-30 16:39:09 +0300

    Add slack integration service spec

--- spec/services/integrations/slack_spec.rb
+++ spec/services/integrations/slack_spec.rb
@@ -3,14 +3,27 @@
 require 'rails_helper'
 
 RSpec.describe Integrations::SlackService do
+  let(:event) { create(:event) }
   let(:integration) { create(:integration, :slack) }
+  before do
+    allow_any_instance_of(Slack::Notifier).to receive(:post).and_return(true)
+  end
 
   subject do
+    described_class.call(event: event, action: action, integration: integration, reason: reason)
   end
 
   context 'create event' do
-    let(:result) {}
+    let(:action) { UserChannel::ACTIONS::CREATE_EVENT }
+    let(:reason) { nil }
 
-    it { is_expected.to eq(result) }
+    it { is_expected.to be_truthy }
+  end
+
+  context 'update event' do
+    let(:action) { UserChannel::ACTIONS::UPDATE_EVENT }
+    let(:reason) { 'Occurrence' }
+
+    it { is_expected.to be_truthy }
   end
 end
