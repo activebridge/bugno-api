e233a53b3c718e52af2c2ee6b79c81f2a35ac2b9
Dmitry Sivoglaz <grayeyed16@gmail.com>
2019-10-24 01:11:43 +0300

    Fix precompile assets

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,1 +1,4 @@
-//= link_directory ../stylesheets .css
+//= link_directory ../stylesheets .scss
+//= link_directory ../javascripts .js
+//= link grape_swagger_rails/application.css
+//= link grape_swagger_rails/application.js
