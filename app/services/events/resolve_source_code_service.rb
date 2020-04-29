3fc3a4422772931463208d6ee6c96a2bbd67b2e9
Dmitry Sivoglaz <grayeyed16@gmail.com>
2020-04-29 19:13:20 +0300

    Add service to resolve source code

---
+++ app/services/events/resolve_source_code_service.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'open-uri'
+
+module Events
+  class ResolveSourceCodeService < ApplicationService
+    def call
+      `node ./app/assets/javascripts/parser.js map:#{source_map.path}
+                                               line:#{@event.backtrace['lineno']}
+                                               column:#{@event.backtrace['colno']}`
+    end
+
+    private
+
+    def source_map
+      @source_map ||= open(source_map_url)
+    end
+
+    def source_map_url
+      @source_map_url ||= "#{@event.backtrace['filename']}.map"
+    end
+  end
+end
