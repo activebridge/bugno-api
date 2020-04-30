b538f8a9e017b3e051aafa973e5f56169e0b4db7
Dmitry Sivoglaz <grayeyed16@gmail.com>
2020-04-30 20:14:36 +0300

    Add bugno ruby to production

--- config/initializers/bugno.rb
+++ config/initializers/bugno.rb
@@ -3,6 +3,7 @@
 Bugno.configure do |config|
   # Specify api key to send exception to Bugno
   config.api_key = ENV['BUGNO_API_KEY']
+  config.api_url = ENV['BUGNO_API_URL'] if Rails.env.production?
 
   # Send in background with threading:
   config.send_in_background = true
@@ -21,7 +22,7 @@ Bugno.configure do |config|
   ]
 
   # Specify or add usage environments:
-  config.usage_environments = %w[staging]
+  config.usage_environments = %w[staging production]
   # config.usage_environments << 'development'
 
   # Specify current user method:
