3ed9c8d4bce296e9e2f07c09c8f72a8cbcae363a
Dmitry Sivoglaz <grayeyed16@gmail.com>
2020-04-30 16:37:43 +0300

    Improve resolve source code service

--- app/services/events/resolve_source_code_service.rb
+++ app/services/events/resolve_source_code_service.rb
@@ -5,35 +5,45 @@ require 'open-uri'
 module Events
   class ResolveSourceCodeService < ApplicationService
     def call
-      resolved_trace
+      unless File.exist?(path)
+        return if download.blank?
+
+        save_source_map
+      end
+      resolve_trace
     end
 
     private
 
-    # rubocop:disable Security/Open
-    def source_map
-      @source_map ||= download.is_a?(File) ? download : IO.copy_stream(download, path)
-    end
-
-    def download
-      @download ||= open(source_map_url)
-    end
-
     def path
-      @path ||= download.is_a?(File) ? source_map.path : "tmp/#{download.base_uri.to_s.split('/')[-1]}"
+      @path ||= "tmp/#{filename}"
     end
+
+    def filename
+      @filename ||= "#{@trace['filename'].split('/')[-1]}.map"
+    end
+
+    def save_source_map
+      download.is_a?(File) ? download : IO.copy_stream(download, path)
+    end
+
+    # rubocop:disable Security/Open
+    def download
+      @download ||= begin
+        open(source_map_url)
+                    rescue StandardError
+                      nil
+      end
+    end
+    # rubocop:enable Security/Open
 
     def source_map_url
-      @source_map_url ||= "#{trace['filename']}.map"
+      @source_map_url ||= "#{@trace['filename']}.map"
     end
 
-    def trace
-      @trace ||= @event.backtrace.first
-    end
-
-    def resolved_trace
-      result = `node #{Constants::Path::SOURCE_MAP_PARSER} map:#{path} line:#{trace['lineno']} column:#{trace['colno']}`
-      parsed = JSON.parse(result)
+    def resolve_trace
+      res = `node #{Constants::Path::SOURCE_MAP_PARSER} map:#{path} line:#{@trace['lineno']} column:#{@trace['colno']}`
+      parsed = JSON.parse(res)
       { colno: parsed['column'], filename: parsed['source'], lineno: parsed['line'], method: parsed['name'] }
     end
   end
