f9d518960e043085b4275fed0bf337a510f140c6
Dmitry Sivoglaz <grayeyed16@gmail.com>
2020-05-01 17:37:43 +0300

    Add assigned_in_project? && assigns_count_in_project

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,12 @@ class User < ActiveRecord::Base
 
   has_many :project_users, dependent: :destroy
   has_many :projects, through: :project_users
+
+  def assigns_count_in_project(project_id)
+    Event.where(project_id: project_id, user_id: id).count
+  end
+
+  def assigned_in_project?(project_id)
+    Event.where(project_id: project_id, user_id: id).any?
+  end
 end
