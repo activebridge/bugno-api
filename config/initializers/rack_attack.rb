f95c28414be64dede5db6bda53759b3374059737
Dmitry Sivoglaz <grayeyed16@gmail.com>
2020-05-06 14:42:51 +0300

    Change limit, period and req type

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -2,5 +2,7 @@
 
 class Rack::Attack
   Rack::Attack.cache.store = ActiveSupport::Cache::MemoryStore.new
-  throttle('req/ip', limit: 60, period: 60.seconds, &:ip)
+  throttle('req/ip', limit: 15, period: 10.seconds) do |req|
+    req.ip if req.post?
+  end
 end
