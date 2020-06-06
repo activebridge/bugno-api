6cd530189a7cbbbf10b4002c6006293a25f25126
Dmitry Sivoglaz <grayeyed16@gmail.com>
2020-06-06 17:16:34 +0300

    Change occrrence notification points, add occurrences limit

--- lib/constants/rules.rb
+++ lib/constants/rules.rb
@@ -2,6 +2,7 @@
 
 module Constants
   module Rules
-    OCCURRENCE_NOTIFICATION_POINTS = [5, 10, 25, 50, 100, 150, 250, 500, 750, 1000, 2000, 3000, 4000]
+    OCCURRENCE_NOTIFICATION_POINTS = [5, 10, 25, 50, 100].freeze
+    OCCURRENCES_LIMIT = 100
   end
 end
