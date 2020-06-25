e0cc36a6c50b03e9d5602427134aa62626fbb536
Dmitry Sivoglaz <grayeyed16@gmail.com>
2020-06-25 18:20:59 +0300

    Add user email and refer url

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -5,12 +5,21 @@ class EventSerializer < ApplicationSerializer
              :message, :backtrace, :framework, :url, :ip_address, :headers,
              :http_method, :params, :position, :server_data, :created_at,
              :parent_id, :person_data, :background_data, :route_params,
-             :updated_at, :occurrence_count, :last_occurrence_at
+             :updated_at, :occurrence_count, :last_occurrence_at, :user_email,
+             :refer_url
 
   attribute :user_agent, if: proc { object.user_agent? }
   belongs_to :user, serializer: PublicUserSerializer
 
   def user_agent
     DeviceDetectorSerializer.new(object.headers['User-Agent']).as_json[:parsed_data]
+  end
+
+  def user_email
+    object.person_data['email']
+  end
+
+  def refer_url
+    object.headers['Referer']
   end
 end
