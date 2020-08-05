2a8fde1575281e24efc2793215713b967d66c147
Dmitry Sivoglaz <grayeyed16@gmail.com>
2020-08-05 22:52:09 +0300

    Move logic to its responsible services

--- app/services/events/create_service.rb
+++ app/services/events/create_service.rb
@@ -2,59 +2,28 @@
 
 class Events::CreateService < ApplicationService
   def call
-    return [{ error: I18n.t('api.errors.invalid_api_key') }, 401] unless project
+    return event unless project
 
-    resolve_source_code if resolve_source_code?
-    return event unless project.with_lock { event.save }
-
-    notify if notify?
-    [{ message: I18n.t('api.event_captured') }, 201]
+    assign_parent
+    project.with_lock { event.save }
+    event
   end
 
   private
 
-  def resolve_source_code?
-    event.framework == Constants::Event::BROWSER_JS && event.backtrace.present?
-  end
-
-  def resolve_source_code
-    result = ::Events::ResolveSourceCodeService.call(trace: event.backtrace[0])
-    event.backtrace.unshift(result) if result
-  end
-
-  def notify?
-    event.parent? || occurred_again?
-  end
-
-  def occurred_again?
-    Constants::Rules::OCCURRENCE_NOTIFICATION_POINTS.any? { |point| point == parent_event&.occurrence_count }
-  end
-
-  def notify
-    mailer = event.parent? ? :exception : :occurrence
-    EventMailer.send(mailer, event, user_emails).deliver_later
-    Integration.notify(notify_attributes)
-  end
-
-  def notify_attributes
-    return { event: event, action: UserChannel::ACTIONS::CREATE_EVENT, reason: nil } if event.parent?
-
-    { event: parent_event, action: UserChannel::ACTIONS::UPDATE_EVENT, reason: 'Occurrence' }
-  end
-
   def project
-    @project ||= Project.find_by(api_key: declared_params[:project_id])
+    @project ||= Project.find_by(api_key: @params[:project_id])
   end
 
   def event
-    @event ||= project.events.new(declared_params)
+    @event ||= Event.new(@params.merge(project: project))
   end
 
-  def parent_event
-    @parent_event ||= event.parent
+  def parent
+    @parent ||= event.parent
   end
 
-  def user_emails
-    @user_emails ||= project.users.pluck(:email)
+  def assign_parent
+    event.parent_id = ::Events::AssignParentService.call(event: event, project: project)
   end
 end
