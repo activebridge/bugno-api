0184fab88185be6886760f6a0f601399904009a1
Dmitry Sivoglaz <grayeyed16@gmail.com>
2020-08-06 00:30:54 +0300

    Fix long line

--- app/services/events/create_service.rb
+++ app/services/events/create_service.rb
@@ -19,6 +19,7 @@ class Events::CreateService < ApplicationService
   end
 
   def event_attributes
-    @event_attributes ||= ::Events::BuildAttributesService.call(params: @params, project: project).merge(project: project)
+    @event_attributes ||= ::Events::BuildAttributesService
+                          .call(params: @params, project: project).merge(project: project)
   end
 end
