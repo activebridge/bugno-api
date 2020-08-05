c1502180fd140e4e0a6fa3937ae7ec4f9c8ff2c3
Dmitry Sivoglaz <grayeyed16@gmail.com>
2020-08-05 22:51:30 +0300

    Rename params && assignee_id

--- app/services/events/update_service.rb
+++ app/services/events/update_service.rb
@@ -2,7 +2,7 @@
 
 class Events::UpdateService < ApplicationService
   def call
-    return unless event.update(declared_params[:event])
+    return unless event.update(params[:event])
 
     notify if event.saved_change_to_status?
     notify_assignee if notify_assignee?
@@ -13,23 +13,23 @@ class Events::UpdateService < ApplicationService
 
   def notify
     action = UserChannel::ACTIONS::UPDATE_EVENT
-    Integration.notify(event: event, action: action, reason: user.nickname)
+    Integration.notify(event: event, action: action, reason: @user.nickname)
   end
 
   def notify_assignee?
-    assignee_user_id = declared_params.dig(:event, :user_id)
-    assignee_user_id.present? && assignee_user_id != user.id && event.saved_change_to_user_id?
+    assignee_id = params.dig(:event, :user_id)
+    assignee_id.present? && assignee_id != @user.id && event.saved_change_to_user_id?
   end
 
   def notify_assignee
-    EventMailer.assign(event, user).deliver_later
+    EventMailer.assign(event, @user).deliver_later
   end
 
   def event
-    @event ||= project.events.find(declared_params[:id])
+    @event ||= project.events.find(params[:id])
   end
 
   def project
-    @project ||= user.projects.find(declared_params[:project_id])
+    @project ||= @user.projects.find(params[:project_id])
   end
 end
