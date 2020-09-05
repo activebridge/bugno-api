0e71a0a2995475ff9d045ffc33733f9efb4faad4
Dmitry Sivoglaz <grayeyed16@gmail.com>
2020-09-05 20:03:46 +0300

    Change create event behavior to replace occurrence after 100 and limit

--- app/services/events/create_service.rb
+++ app/services/events/create_service.rb
@@ -1,14 +1,29 @@
 # frozen_string_literal: true
 
 class Events::CreateService < ApplicationService
-  def call
+  def call # rubocop:disable Metrics/AbcSize
     return event unless project
 
-    project.with_lock { event.save }
+    ::Events::ResolveSourceCodeService.call(event: event, trace: event.backtrace[0]) if resolve_source?
+    occurrence_limit_reached? ? push_occurrence : create_event
+    parent.update(last_occurrence_at: Time.now) if event.occurrence?
     event
   end
 
   private
+
+  def create_event
+    project.with_lock { event.save }
+  end
+
+  def push_occurrence
+    parent.occurrences.first.delete
+    create_event
+  end
+
+  def resolve_source?
+    event.framework == Constants::Event::BROWSER_JS && event.backtrace.present?
+  end
 
   def project
     @project ||= Project.find_by(api_key: @params[:project_id])
@@ -19,20 +34,18 @@ class Events::CreateService < ApplicationService
   end
 
   def event_attributes
-    return @params unless project
-
-    parent_muted? ? cut_attributes : built_attributes
+    project ? built_attributes : @params
   end
 
-  def parent_muted?
-    built_attributes[:parent_id] && Event.find(built_attributes[:parent_id]).muted?
-  end
-
-  def cut_attributes
-    built_attributes.slice(:id, :project_id, :title, :message, :framework, :parent_id).merge(status: :muted)
+  def parent
+    @parent ||= Event.find(built_attributes[:parent_id])
   end
 
   def built_attributes
     @built_attributes ||= ::Events::BuildAttributesService.call(params: @params, project: project)
+  end
+
+  def occurrence_limit_reached?
+    built_attributes[:parent_id] && parent.occurrence_limit_reached?
   end
 end
