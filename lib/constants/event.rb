2923f0a39a4fd2d128ac4bd86c63449a68e49091
Dmitry Sivoglaz <grayeyed16@gmail.com>
2020-09-06 03:06:31 +0300

    Feeeze OCCURRENCE_NOTIFICATION_POINTS

--- lib/constants/event.rb
+++ lib/constants/event.rb
@@ -4,6 +4,6 @@ module Constants
   module Event
     BROWSER_JS = 'browser-js'
     OCCURRENCE_LIMIT = 100
-    OCCURRENCE_NOTIFICATION_POINTS = [5, 10, 25, 50, 100, 150, 250, 500, 750, 1000, 2000, 3000, 4000]
+    OCCURRENCE_NOTIFICATION_POINTS = [5, 10, 25, 50, 100, 150, 250, 500, 750, 1000, 2000, 3000, 4000].freeze
   end
 end
