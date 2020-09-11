1b959ff36cff31e4796cde8753449ac58dd6262a
Dmitry Sivoglaz <grayeyed16@gmail.com>
2020-09-11 17:23:38 +0300

    Add message similarity percent to constants

--- lib/constants/event.rb
+++ lib/constants/event.rb
@@ -5,5 +5,6 @@ module Constants
     BROWSER_JS = 'browser-js'
     OCCURRENCE_LIMIT = 100
     OCCURRENCE_NOTIFICATION_POINTS = [5, 10, 25, 50, 100, 150, 250, 500, 750, 1000, 2000, 3000, 4000].freeze
+    MESSAGE_SIMILARITY_PERCENT = 0.1
   end
 end
