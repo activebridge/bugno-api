9b420e35dbd33fd93c176e4e3f30a45d01bc9a43
Dmitry Sivoglaz <grayeyed16@gmail.com>
2021-01-31 23:34:42 +0200

    Add logic for hight frequency to notify_service

--- app/services/events/notify_service.rb
+++ app/services/events/notify_service.rb
@@ -4,23 +4,46 @@ class Events::NotifyService < ApplicationService
   def call
     return unless notify? && notifiable_status?
 
-    mailer = @event.parent? ? :exception : :occurrence
-    EventMailer.send(mailer, @event, user_emails).deliver_later
+    EventMailer.send(mailer_action, @event, user_emails).deliver_later
     Integration.notify(notify_attributes)
   end
 
   private
 
+  def mailer_action
+    @mailer_action ||= begin
+      return :high_frequency if high_frequency?
+
+      @event.parent? ? :exception : :occurrence
+    end
+  end
+
   def notify?
-    @event.parent? || occurrence_point?
+    @event.parent? || notify_high_frequency? || occurrence_point?
   end
 
   def notifiable_status?
     !@event.muted? && !@event.parent&.muted?
   end
 
+  def notification_point?(times_occurred)
+    Constants::Event::OCCURRENCE_NOTIFICATION_POINTS.any? { |point| point == times_occurred }
+  end
+
   def occurrence_point?
-    Constants::Event::OCCURRENCE_NOTIFICATION_POINTS.any? { |point| point == @event.parent.occurrence_count }
+    notification_point?(@event.parent.occurrence_count) && !high_frequency?
+  end
+
+  def frequency
+    @frequency ||= Event.since(1.minute.ago).where(title: @event.title).count
+  end
+
+  def high_frequency?
+    @high_frequency ||= frequency > Constants::Event::FIRST_NOTIFICATION_POINT - 1
+  end
+
+  def notify_high_frequency?
+    notification_point?(frequency)
   end
 
   def notify_attributes
