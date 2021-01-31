f70968a8ea2515ce52b77d7dcab8e5e135209083
Dmitry Sivoglaz <grayeyed16@gmail.com>
2021-01-31 23:33:49 +0200

    Add FIRST_NOTIFICATION_POINT

--- lib/constants/event.rb
+++ lib/constants/event.rb
@@ -4,7 +4,9 @@ module Constants
   module Event
     BROWSER_JS = 'browser-js'
     OCCURRENCE_LIMIT = 100
-    OCCURRENCE_NOTIFICATION_POINTS = [5, 10, 25, 50, 100, 150, 250, 500, 750, 1000, 2000, 3000, 4000].freeze
+    FIRST_NOTIFICATION_POINT = 5
+    OCCURRENCE_NOTIFICATION_POINTS = [FIRST_NOTIFICATION_POINT, 10, 25, 50, 100, 150, 250,
+                                      500, 750, 1000, 2000, 3000, 4000].freeze
     MESSAGE_SIMILARITY_PERCENT = 0.1
   end
 end
