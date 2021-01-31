adf1d899594397243de4dec6c4e7ee38d03223e8
Dmitry Sivoglaz <grayeyed16@gmail.com>
2021-01-31 23:34:30 +0200

    Add high frequency mailer

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -36,6 +36,17 @@ class EventMailer < ApplicationMailer
                                        title: @event.title, assigner_name: @assigner.nickname))
   end
 
+  def high_frequency(event, addresses)
+    @event = event
+    @link = project_link
+    @frequency = Event.since(1.minute.ago).where(title: @event.title).count
+    add_high_frequency_image
+    mail(to: addresses,
+         subject: default_i18n_subject(project_name: @event.project.name,
+                                       environment: @event.environment,
+                                       title: @event.title, frequency: @frequency))
+  end
+
   private
 
   def assign_action_name
@@ -52,9 +63,17 @@ class EventMailer < ApplicationMailer
     attachments.inline['bugno-logo.png'] = File.read('app/assets/images/bugno-logo.png')
   end
 
+  def add_high_frequency_image
+    attachments.inline['high_frequency.png'] = File.read('app/assets/images/high_frequency.png')
+  end
+
   def link
     id = @event.parent? ? @event.id : @parent_event.id
     "#{I18n.t("web_url.#{Rails.env}")}/projects/#{@event.project.slug}/event/#{id}"
+  end
+
+  def project_link
+    "#{I18n.t("web_url.#{Rails.env}")}/projects/#{@event.project.slug}"
   end
 
   def chunk_of_code
