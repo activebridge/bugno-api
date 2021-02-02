2131f6ab47e4a215dea2ec20d09568a9e41595f3
Dmitry <grayeyed16@gmail.com>
2021-02-02 03:35:28 +0200

    High frequency alerts mailer (#117)
    * Add high_frequency image
    
    * Add .high-frequency-image
    
    * Add FIRST_NOTIFICATION_POINT
    
    * Add #since scope to Event model
    
    * Add locales for high frequency mailer
    
    * Add high frequency mailer
    
    * Add logic for hight frequency to notify_service
    
    * Add project_id to frequency query
    
    * Add project_id to query
    
    * Move query to method
    
    * Fix condition for #notify?
    
    * Refactor condition in notify_service
    
    * Even more refactor to #notify
    
    * Remove redundant method
    
    * Remove blank line

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -36,7 +36,22 @@ class EventMailer < ApplicationMailer
                                        title: @event.title, assigner_name: @assigner.nickname))
   end
 
+  def high_frequency(event, addresses)
+    @event = event
+    @link = project_link
+    @frequency = frequency
+    add_high_frequency_image
+    mail(to: addresses,
+         subject: default_i18n_subject(project_name: @event.project.name,
+                                       environment: @event.environment,
+                                       title: @event.title, frequency: @frequency))
+  end
+
   private
+
+  def frequency
+    Event.since(1.minute.ago).where(title: @event.title, project_id: @event.project_id).count
+  end
 
   def assign_action_name
     @action_name = action_name
@@ -52,9 +67,17 @@ class EventMailer < ApplicationMailer
     attachments.inline['bugno-logo.png'] = File.read('app/assets/images/bugno-logo.png')
   end
 
+  def add_high_frequency_image
+    attachments.inline['high_frequency.png'] = File.read('app/assets/images/high_frequency.png')
+  end
+
   def link
     id = @event.parent? ? @event.id : @parent_event.id
     "#{I18n.t("web_url.#{Rails.env}")}/projects/#{@event.project.slug}/event/#{id}"
+  end
+
+  def project_link
+    "#{I18n.t("web_url.#{Rails.env}")}/projects/#{@event.project.slug}"
   end
 
   def chunk_of_code
