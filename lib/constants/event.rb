6c283c3f10d03c3fa109338da4e11f72974975e1
Dmitry <grayeyed16@gmail.com>
2021-04-19 12:39:35 +0300

    Remove occurrence limit (#121)
    * Move dotenv-rails to dev & test groups
    
    * Remove occurrence_limit logic
    
    * Update mimemagic

--- lib/constants/event.rb
+++ lib/constants/event.rb
@@ -3,7 +3,6 @@
 module Constants
   module Event
     BROWSER_JS = 'browser-js'
-    OCCURRENCE_LIMIT = 100
     FIRST_NOTIFICATION_POINT = 5
     OCCURRENCE_NOTIFICATION_POINTS = [FIRST_NOTIFICATION_POINT, 10, 25, 50, 100, 150, 250,
                                       500, 750, 1000, 2000, 3000, 4000].freeze
